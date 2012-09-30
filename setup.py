19c46f99e886d454e0d5d7ebb3b319c86babe627
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2012-09-30 12:48:49 +0200

    Added a long description to the python package

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@
 
 from setuptools import setup, find_packages
 
+long_description = open('README.md').read()
+
 setup(
     name='booby',
     version='0.1',
     description='A standalone data modeling and validation Python framework',
+    long_description=long_description,
     author='Jaime Gil de Sagredo Luna',
     author_email='jaimegildesagredo@gmail.com',
     packages=find_packages(exclude=['tests', 'tests.*']),
