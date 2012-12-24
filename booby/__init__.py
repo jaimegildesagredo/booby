f8e456d3b747758826bd959f5bc9c4d24460f6ec
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2012-12-24 21:37:56 +0100

    Removed old field classes and added the new Field

--- booby/__init__.py
+++ booby/__init__.py
@@ -16,6 +16,5 @@
 
 
 from booby.models import Model, EmbeddedModel
-from booby.fields import StringField, IntegerField, BoolField
 
-__all__ = ['Model', 'EmbeddedModel', 'StringField', 'IntegerField', 'BoolField']
+__all__ = ['Model', 'EmbeddedModel']
--- booby/fields.py
+++ booby/fields.py
@@ -14,74 +14,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from booby import validators as builtin_validators
+
 
 class Field(object):
-    pass
+    def __init__(self, default=None, validators=[], **kwargs):
+        self.default = default
 
+        # Setup field validators
+        self.validators = []
 
-class TypeField(Field):
-    def __init__(self, **kwargs):
-        self.name = None
-        self.required = kwargs.get('required', False)
+        if kwargs.get('required'):
+            self.validators.append(builtin_validators.Required())
 
-        try:
-            choices = kwargs.get('choices', [])
-            self.choices = set(self.validation(x) for x in choices)
-        except ValueError:
-            raise ValueError('Invalid choices: {0}'.format(choices))
-        except TypeError:
-            raise TypeError("'choices' is not iterable")
+        choices = kwargs.get('choices')
 
-        try:
-            default = kwargs.get('default')
-            self.default = self.validation(default) if default is not None else default
-        except ValueError:
-            raise ValueError('Invalid default value: {0}'.format(default))
+        if choices:
+            self.validators.append(builtin_validators.In(choices))
+
+        self.validators.extend(validators)
 
     def __get__(self, instance, owner):
         if instance is not None:
             return instance._data.get(self, self.default)
         return self
 
-    def __set__(self, instance, value):
-        instance._data[self] = self.validate(value)
-
     def validate(self, value):
-        if value is None:
-            if self.required:
-                raise ValueError("Field '{0}' is required".format(self.name))
-            return value
-
-        if self.choices and value not in self.choices:
-            raise ValueError("Invalid value for field '{0}': {1}".format(
-                self.name, value))
-
-        return self.validation(value)
-
-    def validation(self, value):
-        raise NotImplementedError()
-
-
-class StringField(TypeField):
-    def validation(self, value):
-        if not isinstance(value, basestring):
-            raise ValueError("Invalid value for field '{0}': {1}".format(
-                self.name, value))
-        return value
-
-
-class IntegerField(TypeField):
-    def validation(self, value):
-        try:
-            return int(value)
-        except ValueError:
-            raise ValueError("Invalid value for field '{0}': {1}".format(
-                self.name, value))
-
-
-class BoolField(TypeField):
-    def validation(self, value):
-        if not isinstance(value, (bool, int)):
-            raise ValueError("Invalid value for field '{0}': {1}".format(
-                self.name, value))
-        return bool(value)
+        for validator in self.validators:
+            validator.validate(value)
