797c7daf41251686e213ba79e195d546905f1044
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2012-12-29 22:48:46 +0100

    Added model declaration integration tests

---
+++ tests/integration/test_model_declaration.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from nose.tools import assert_raises_regexp
+
+from booby import errors
+from booby.models import Model
+from booby.fields import StringField, IntegerField, EmbeddedField, EmailField
+
+
+class TestModelValidation(object):
+    def test_when_login_is_none_then_raises_validation_error(self):
+        user = User()
+
+        with assert_raises_regexp(errors.ValidationError, 'required'):
+            user.validate()
+
+    def test_when_karma_is_not_an_integer_then_raises_validation_error(self):
+        user = User(login=u'root', karma=u'max')
+
+        with assert_raises_regexp(errors.ValidationError, 'should be an integer'):
+            user.validate()
+
+    def test_when_token_key_is_not_a_string_then_raises_validation_error(self):
+        user = User(login=u'root', token=Token(key=1))
+
+        with assert_raises_regexp(errors.ValidationError, 'should be a string'):
+            user.validate()
+
+    def test_when_email_is_an_invalid_email_then_raises_validation_error(self):
+        user = User(login=u'root', email='root@localhost')
+
+        with assert_raises_regexp(errors.ValidationError, 'should be a valid email'):
+            user.validate()
+
+
+class Token(Model):
+    key = StringField()
+    secret = StringField()
+
+
+class User(Model):
+    login = StringField(required=True)
+    email = EmailField()
+    name = StringField()
+    karma = IntegerField()
+    token = EmbeddedField(Token)
