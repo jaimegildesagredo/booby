ce8f281f65415ac220ba52df4d0d66dc16ca53c9
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2013-03-07 20:48:12 +0100

    Added Python 3.3 and 2.7 classifiers

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ]
     )
