9a13ecffd5e5312bca419977ac145409a4ebf531
cr0hn <cr0hn@cr0hn.com>
2016-09-16 23:22:41 +0200

    added URI data type

--- booby/fields.py
+++ booby/fields.py
@@ -244,7 +244,7 @@ class URI(String):
     """:class:`Field` subclass with builtin `ip` validation."""
 
     def __init__(self, *args, **kwargs):
-        super(URI, self).__init__(builtin_validators.URL(), *args, **kwargs)
+        super(URI, self).__init__(builtin_validators.URI(), *args, **kwargs)
 
 
 class Raw(Field):
